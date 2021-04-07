d40b8d124360c44ae7a6c1a3ba9a259ef79d76f5
adamSales <asales@wpi.edu>
2021-04-07 14:19:08 -0500

    separate file for fitting models

---
+++ Cognitive Tutor/lca/fitMods.r
@@ -0,0 +1,45 @@
+library(loo)
+library(rstan)
+options(mc.cores = parallel::detectCores())
+rstan_options(auto_write = TRUE)
+library(flextable)
+library(officer)
+library(tidyverse)
+
+load('smallSdat.RData')
+
+#mod <- stan('lca2class2.stan',data=sdat,iter=1000,sample_file='samp.txt',refresh=10)
+
+mod <- stan_model('lca2class2.stan')
+
+sdat$sigStud <- 0.001
+fit0 <- optimizing(mod,data=sdat,hessian=TRUE)#,draws=200,importance_resampling=TRUE)
+
+sdat$sigStud <- 0.5
+fit.5 <- optimizing(mod,data=sdat,hessian=TRUE)#,draws=200,importance_resampling=TRUE)
+
+sdat$sigStud <- 1
+fit1 <- optimizing(mod,data=sdat,hessian=TRUE)#,draws=200,importance_resampling=TRUE)
+
+sdat$sigStud <- 2
+fit2 <- optimizing(mod,data=sdat,hessian=TRUE)#,draws=2000,importance_resampling=TRUE)
+
+sdat$sigStud <- 5
+fit5 <- optimizing(mod,data=sdat,hessian=TRUE)#,draws=2000,importance_resampling=TRUE)
+
+save(fit0,fit.5,fit1,fit2,fit5,sdat,file='mod.RData')
+
+oneClassMod <- stan_model('lca1class.stan')
+fit1class <- optimizing(oneClassMod,data=sdat,hessian=TRUE,draws=2000,importance_resampling=TRUE)
+save(fit1class,sdat,oneClassMod,file='fit1class.RData')
+
+#### no student effects or covariates
+mod0 <- stan_model('lca2class0.stan')
+fit00 <- optimizing(mod0,data=sdat,hessian=TRUE,draws=2000,importance_resampling=TRUE)
+
+#### yes student effects no covariates
+mod1 <- stan_model('lca2class1.stan')
+sdat$sigStud <- 1
+fit01 <- optimizing(mod1,data=sdat,hessian=TRUE,draws=2000,importance_resampling=TRUE)
+
+save(fit00,fit01,mod0,mod1,sdat,file='prelimMods.RData')
